5c0d7dc9a96b317f30d6ff8f376b9648d5466b47
OleksandrDanylchenko <olexander.danilchenko@gmail.com>
2022-05-01 13:30:35 +0300

    feat: add local apps CORS rules

--- src/main.ts
+++ src/main.ts
@@ -1,8 +1,13 @@
 import { NestFactory } from '@nestjs/core';
+
 import { AppModule } from './app.module';
 
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
-  await app.listen(3000);
+  app.enableCors({
+    origin: [/localhost:(3000|3100)/], // Local development addresses for the admin & emitter apps
+  });
+
+  await app.listen(3200);
 }
 bootstrap();
