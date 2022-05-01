20385d81822af74996e81946dea5a3e81b557d05
OleksandrDanylchenko <olexander.danilchenko@gmail.com>
2022-05-01 15:46:57 +0300

    feat: add mongo env variables validation

--- src/config/config.schema.ts
+++ src/config/config.schema.ts
@@ -3,11 +3,18 @@ import * as Joi from 'joi';
 export const configValidationSchema = Joi.object({
   PORT: Joi.number().default(3300),
   STAGE: Joi.string().required(),
+  JWT_SECRET: Joi.string().required(),
+
   PG_DB_HOST: Joi.string().required(),
   PG_DB_PORT: Joi.number().default(5432),
   PG_DB_USERNAME: Joi.string().required(),
   PG_DB_PASSWORD: Joi.string().required(),
   PG_DB_DATABASE: Joi.string().required(),
   PG_DB_MIGRATIONS_FOLDER: Joi.string().required(),
-  JWT_SECRET: Joi.string().required(),
+
+  MONGO_DB_HOST: Joi.string().required(),
+  MONGO_DB_PORT: Joi.number().default(27017),
+  MONGO_DB_USERNAME: Joi.string().required(),
+  MONGO_DB_PASSWORD: Joi.string().required(),
+  MONGO_DB_DATABASE: Joi.string().required(),
 });
