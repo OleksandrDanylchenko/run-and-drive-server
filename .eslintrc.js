d8687741d7509ccfb9ace848e1961d0f50912545
OleksandrDanylchenko <olexander.danilchenko@gmail.com>
2022-05-01 15:22:42 +0300

    feat: add aliases for the import paths

--- .eslintrc.js
+++ .eslintrc.js
@@ -1,17 +1,31 @@
 module.exports = {
+  root: true,
   parser: '@typescript-eslint/parser',
   parserOptions: {
     project: 'tsconfig.json',
     sourceType: 'module',
   },
-  plugins: ['@typescript-eslint/eslint-plugin'],
+  plugins: ['import', '@typescript-eslint/eslint-plugin'],
   extends: [
     'plugin:@typescript-eslint/recommended',
     'plugin:import/recommended',
     'plugin:import/typescript',
     'plugin:prettier/recommended',
   ],
-  root: true,
+  settings: {
+    'import/parsers': {
+      '@typescript-eslint/parser': ['.ts', '.tsx'],
+    },
+    'import/resolver': {
+      node: {
+        paths: ['src'],
+        extensions: ['.js', '.jsx', '.ts', '.tsx'],
+      },
+      typescript: {
+        alwaysTryTypes: true,
+      },
+    },
+  },
   env: {
     node: true,
     jest: true,
--- src/app.module.ts
+++ src/app.module.ts
@@ -1,12 +1,11 @@
 import { Module } from '@nestjs/common';
 import { ConfigModule } from '@nestjs/config';
 
+import { configValidationSchema } from '@config/config.schema';
+import { PostgresDatabaseModule } from '@database/pg.module';
+
 import { AppController } from './app.controller';
 import { AppService } from './app.service';
-import { configValidationSchema } from './config/config.schema';
-import { PostgresDatabaseModule } from './database/pg.module';
-
-console.log(process.env.STAGE, `.env.stage.${process.env.STAGE}`);
 
 @Module({
   imports: [
